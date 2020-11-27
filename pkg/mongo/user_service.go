54b11359dd4316c410b599ed10dfde17afd72a42
rezaghanbari <ghanbari.reza94@gmail.com>
2020-11-27 04:29:41 +0330

    add hash to user service

--- pkg/mongo/user_service.go
+++ pkg/mongo/user_service.go
@@ -9,22 +9,31 @@ import (
 // UserService : user service struct 
 type UserService struct {
 	collection *mgo.Collection
+	crypto 		root.Crypto
 }
 
 // NewUserService creating a new user service
 // The constructor gets a collection from the 
 // session parameter and sets up the user index.
-func NewUserService(session *Session, dbName string, collectionName string) *UserService {
+func NewUserService(session *Session, dbName string, collectionName string, crypto root.Crypto) *UserService {
 	collection := session.GetCollection(dbName, collectionName)
 	collection.EnsureIndex(userModelIndex())
-	return &UserService{collection}
+	return &UserService{collection, crypto}
 }
 
 // CreateUser function is responsible for 
 // creating a new user model and insert into the db
 func (p *UserService) CreateUser(u *root.User) error {
 	user := newUserModel(u)
+	hashedPassword, err := p.crypto.Generate(user.Password)
+
+	if err != nil {
+		return err
+	}
+	user.Password = hashedPassword
 	return p.collection.Insert(&user)
+
+
 }
 
 // GetByUsername is responsible for getting a specific 
